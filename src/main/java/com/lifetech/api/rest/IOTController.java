fb50643bc348531f5c8ffe0db28648900c1b2f8f
davidlbd <davidlambourdiere@gmail.com>
2019-12-12 15:55:33 +0100

    clean merge with 'master'

--- src/main/java/com/lifetech/api/rest/IOTController.java
+++ src/main/java/com/lifetech/api/rest/IOTController.java
@@ -3,14 +3,7 @@ package com.lifetech.api.rest;
 import com.lifetech.application.dto.IOTDTO;
 import com.lifetech.application.manager.IOTManager;
 import org.springframework.beans.factory.annotation.Autowired;
-<<<<<<< HEAD
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-=======
 import org.springframework.web.bind.annotation.*;
->>>>>>> master
 
 import java.util.List;
 
