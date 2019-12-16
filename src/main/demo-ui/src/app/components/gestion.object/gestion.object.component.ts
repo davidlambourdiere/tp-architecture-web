c49afd649334db170f37b4f50f87c947ae68f7eb
Hella Bessaoud <bsd.hella@yahoo.com>
2019-12-16 16:12:00 +0100

    BO Heater

--- src/main/demo-ui/src/app/components/gestion.object/gestion.object.component.ts
+++ src/main/demo-ui/src/app/components/gestion.object/gestion.object.component.ts
@@ -1,14 +1,42 @@
-import {Component} from "@angular/core";
+
+import {Component, OnInit} from '@angular/core';
+import {PersonDTO} from "../../dto/PersonDTO";
+import {PersonService} from "../../service/PersonService";
+import {ActivatedRoute, Router} from "@angular/router";
+import {AuthService} from "../../service/AuthService";
+// @ts-ignore
+import {HeaterService} from "../../service/HeaterService";
+import {HeaterDTO} from "../../dto/HeaterDTO";
+
+
 
 @Component({
   selector: 'gestion-object',
   templateUrl: './gestion.object.component.html',
   styleUrls: ['./gestion.object.component.scss']
 })
-export class  GestionObjectComponent {
 
+
+
+export class GestionObjectComponent implements OnInit {
+  heaters: HeaterDTO = new HeaterDTO();
+
+
+  constructor(private personService: PersonService, private router: Router, private route: ActivatedRoute, private heaterService: HeaterService) {
+  }
+
+  ngOnInit(): void {
+    this.showHeater();
+  }
+
+  showHeater() {
+    this.route.params.subscribe(params => {
+      this.heaterService.findAllHeater().subscribe(data => {
+        this.heaters = data;
+
+      })
+    })
+
+
+  }
 }
-
-var timeControl = document.querySelector('input[type="time"]');
-
-
---
+++ src/main/demo-ui/src/app/service/HeaterService.ts
@@ -0,0 +1,28 @@
+import {Injectable} from "@angular/core";
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {IOTDTO} from "../dto/IOTDTO";
+import {HeaterDTO} from "../dto/HeaterDTO";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HeaterService {
+  constructor(private http: HttpClient) {
+  }
+
+  findAllIOT(): Observable<IOTDTO> {
+    return this.http.get<IOTDTO>(`api/iots`);
+  }
+
+  findAllHeater(): Observable<HeaterDTO> {
+    return this.http.get<HeaterDTO>(`api/heaters`);
+  }
+  findIOTByType(type: string): Observable<IOTDTO> {
+    return this.http.post<IOTDTO>(`api/iots/findByType`, type);
+  }
+
+  findIOTByPerson(idperson: string): Observable<any> {
+    return this.http.get(`api/iots/findByPerson/${idperson}`);
+  }
+}
