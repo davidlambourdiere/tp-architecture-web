bf6ff32e239dafb84e7a811d593642d1464716b7
Kadia Toure <kadiditoure@gmail.com>
2020-01-27 16:51:54 +0100

    updated healthhistoric and strap

--- src/main/resources/db/migration/V1.0.4__add_historic.sql
+++ src/main/resources/db/migration/V1.0.4__add_historic.sql
@@ -6,6 +6,5 @@ systolic VARCHAR(255),
 diastolic VARCHAR(255),
 sugarlevel VARCHAR(255),
 stepcounter VARCHAR(255),
-strap_id BIGINT UNSIGNED NOT NULL,
-CONSTRAINT FK_healthhistoric_strap_id FOREIGN KEY (strap_id) REFERENCES strap(id)
+strapid VARCHAR(255)
 );
