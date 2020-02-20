ac4022f386df2c02293717113af25d410a0aa768
Mohamed Emam <mohamed.emam93@outlook.fr>
2020-02-20 11:31:32 +0100

    Creation du component "confort"
    Ajout du component "confort" qui affiche les informations de cet abonnement si le profile du résident est du type confort

---
+++ src/main/demo-ui/src/app/components/subscription/confort/confort.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import {SubscriptionDTO} from "../../../dto/SubscriptionDTO";
+import {ActivatedRoute, Router} from "@angular/router";
+import {SubscriptionService} from "../../../service/SubscriptionService";
+
+@Component({
+  selector: 'app-confort',
+  templateUrl: './confort.component.html',
+  styleUrls: ['./confort.component.scss']
+})
+export class ConfortComponent implements OnInit {
+  subscriptions: SubscriptionDTO = new SubscriptionDTO();
+  name: string;
+
+  constructor(private router: Router, private route: ActivatedRoute, private subservice: SubscriptionService) {
+  }
+
+  ngOnInit() {
+    this.name = 'confort';
+    console.log(JSON.stringify(this.name));
+    this.showSubscriptionConfort();
+  }
+
+  showSubscriptionConfort() {
+    this.route.params.subscribe(params => {
+      this.subservice.findSubscriptionByProfile(this.name).subscribe(data => {
+        this.subscriptions = data;
+
+      })
+    })
+  }
+
+}
+
