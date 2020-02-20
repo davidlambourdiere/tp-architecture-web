f7d7455d8a37250edf9f6e8b2527ef2874775682
Mohamed Emam <mohamed.emam93@outlook.fr>
2020-02-20 11:04:28 +0100

    Update de la method verifyConnection dans PersonService
    Ajout d'une boucle for qui check le profile du resident lorsqu'il se connecte sur l'interface

--- src/main/java/com/lifetech/domain/service/PersonServiceImpl.java
+++ src/main/java/com/lifetech/domain/service/PersonServiceImpl.java
@@ -15,6 +15,13 @@ public class PersonServiceImpl implements PersonService{
                     return personToCheck;
                 }
             }
+
+        }
+        for (Person profileToCheck: people){
+            if(profileToCheck.getProfileType().equals(person.getProfileType())){
+                return profileToCheck;
+            }
+
         }
         return null;
     }
