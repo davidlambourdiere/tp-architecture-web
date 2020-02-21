81da0cb23fc67587c585ab3e55298ebdbfe4c974
Mohamed Emam <mohamed.emam93@outlook.fr>
2020-02-21 18:24:38 +0100

    Modification de nom de vatiable dans la methode createSuscription
    Modification de nom de variable 's" par 'createSubscription'

--- src/main/java/com/lifetech/api/rest/SubscriptionController.java
+++ src/main/java/com/lifetech/api/rest/SubscriptionController.java
@@ -33,8 +33,8 @@ public class SubscriptionController extends RestBaseController {
     }
 
     @PostMapping("subscription/createSubscription")
-    private SubscriptionDTO createSubscription(@Valid @RequestBody SubscriptionDTO s) {
-        return subscriptionManager.save(s);
+    private SubscriptionDTO createSubscriptionByResident(@Valid @RequestBody SubscriptionDTO createSubscription) {
+        return subscriptionManager.save(createSubscription);
     }
 
 
