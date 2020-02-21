7e140244ac7d6fe4947c8fbc5ac56edb259da0e3
Mohamed Emam <mohamed.emam93@outlook.fr>
2020-02-21 18:28:45 +0100

    Ajout des tables referencesIOT et referencesServices

--- src/main/resources/db/migration/V1.0.2__add_subriscription.sql
+++ src/main/resources/db/migration/V1.0.2__add_subriscription.sql
@@ -17,6 +17,26 @@ amount float,
 programdate int,
 primary key(id, subscriptionid, personid),
 CONSTRAINT fk_subscription_bill_id foreign key (subscriptionid) references subscription (id),
-CONSTRAINT fk_subscription_person_id foreign key (personid) references person (id)
+CONSTRAINT fk_subscription_person_id foreign key (personid) references person(id)
 );
+
+create table referencesIOT(
+id SERIAL PRIMARY KEY,
+numSerie VARCHAR(255),
+type VARCHAR(255),
+brand VARCHAR(255),
+price DOUBLE
+);
+
+create table referencesServices(
+id SERIAL PRIMARY KEY,
+name VARCHAR(255),
+description VARCHAR(255),
+price DOUBLE,
+);
+
+
+
+
+)
 
