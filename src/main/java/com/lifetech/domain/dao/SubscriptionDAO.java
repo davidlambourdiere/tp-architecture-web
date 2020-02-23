f99dc29be26c8e54197bd84fa14103073ac63167
Mohamed Emam <mohamed.emam93@outlook.fr>
2020-02-23 20:19:44 +0100

    Ajout de la prise en compte des services dans le calcul du prix de l'abonnement

--- src/main/java/com/lifetech/domain/dao/SubscriptionDAO.java
+++ src/main/java/com/lifetech/domain/dao/SubscriptionDAO.java
@@ -11,16 +11,16 @@ public interface SubscriptionDAO extends JpaRepository<Subscription, Long> {
 
     Subscription findByName(String subscriptionName);
 
-    @Query(value = "SELECT SUM(strap.price + heater.price)  FROM Strap,Heater", nativeQuery = true)
+    @Query(value = "SELECT SUM(strap.price + heater.price+references_prices.price) FROM Strap,Heater,References_prices where references_prices.name_service ='sport'", nativeQuery = true)
     public Double sumProfileEssentialSecure();
 
-    @Query(value = "SELECT SUM(light.price + heater.price + strap.price)  FROM Light,Heater,Strap",nativeQuery=true )
+    @Query(value = "SELECT SUM(heater.price + strap.price + references_prices.price )  FROM Heater,Strap,References_prices where references_prices.num_serie='S01'",nativeQuery=true )
     public  Double sumProfileSerenity();
 
-    @Query(value = "SELECT SUM(light.price + heater.price + strap.price) AS price FROM Light,Heater,Strap ",nativeQuery = true)
+    @Query(value = "SELECT SUM(light.price + heater.price + strap.price+ shutter.price + references_prices.price) FROM Light,Heater,Strap,Shutter,References_prices where references_prices.num_serie='A01' ",nativeQuery = true)
     public Double sumProfileConfort();
 
-    @Query(value = "SELECT SUM(light.price + shutter.price + heater.price + strap.price)  FROM Light,Shutter,Heater,Strap ",nativeQuery = true)
+    @Query(value = "SELECT SUM(light.price + shutter.price + heater.price + strap.price + clock.price + references_prices.price)  FROM Light,Shutter,Heater,Strap,Clock,References_prices where references_prices.name_service='bingo' ",nativeQuery = true)
     public Double sumProfileTotalConfort();
 
 }
