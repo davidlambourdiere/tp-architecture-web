137ce8902b7b0c5dc43db41063e5214662998bfe
Mohamed Emam <mohamed.emam93@outlook.fr>
2020-02-23 17:53:19 +0100

    Modifitication de la structure de la methode findSubscriptionByProfile

--- src/main/java/com/lifetech/application/manager/SubscriptionManager.java
+++ src/main/java/com/lifetech/application/manager/SubscriptionManager.java
@@ -1,6 +1,7 @@
 package com.lifetech.application.manager;
 
 import com.lifetech.application.dto.SubscriptionDTO;
+import com.lifetech.domain.model.Subscription;
 
 import java.util.List;
 
@@ -13,7 +14,10 @@ public interface SubscriptionManager {
 
     boolean delete(SubscriptionDTO deletedSubscription);
 
-    SubscriptionDTO findSubscriptionByProfile(String name);
+    SubscriptionDTO findSubscriptionByProfile(String subscriptionName);
+
+    //Dont work for the moment
+   // SubscriptionDTO sumPrice(String subscriptionName, List<String> typeList, List<String> serviceList);
 
 
 }
--- src/main/java/com/lifetech/application/manager/SubscriptionManagerImp.java
+++ src/main/java/com/lifetech/application/manager/SubscriptionManagerImp.java
@@ -2,7 +2,9 @@ package com.lifetech.application.manager;
 
 import com.lifetech.application.dto.SubscriptionDTO;
 import com.lifetech.domain.OrikaBeanMapper;
+import com.lifetech.domain.dao.ReferencesPricesDAO;
 import com.lifetech.domain.dao.SubscriptionDAO;
+import com.lifetech.domain.model.ReferencesPrices;
 import com.lifetech.domain.model.Subscription;
 import org.springframework.stereotype.Service;
 
@@ -12,10 +14,13 @@ import java.util.List;
 public class SubscriptionManagerImp implements SubscriptionManager{
     private final SubscriptionDAO subscriptionDAO;
 
+    private final ReferencesPricesDAO referencesPricesDAO ;
+
     private final OrikaBeanMapper orikaBeanMapper;
 
-    public SubscriptionManagerImp(SubscriptionDAO subscriptionDAO, OrikaBeanMapper orikaBeanMapper) {
+    public SubscriptionManagerImp(SubscriptionDAO subscriptionDAO, ReferencesPricesDAO referencesPricesDAO, OrikaBeanMapper orikaBeanMapper) {
         this.subscriptionDAO = subscriptionDAO;
+        this.referencesPricesDAO = referencesPricesDAO;
         this.orikaBeanMapper = orikaBeanMapper;
     }
 
@@ -50,38 +55,29 @@ public class SubscriptionManagerImp implements SubscriptionManager{
 
 
    public SubscriptionDTO findSubscriptionByProfile(String name) {
-        SubscriptionDTO subToReturn = new SubscriptionDTO();
+        SubscriptionDTO subToReturn;
         switch(name){
             case "essential":
                 Subscription essential = subscriptionDAO.findByName(name);
                 essential.setPrice(subscriptionDAO.sumProfileEssentialSecure());
-                return orikaBeanMapper.map(essential, SubscriptionDTO.class);
-
+                subToReturn =  orikaBeanMapper.map(essential, SubscriptionDTO.class);
+                break ;
             case "serenity":
                 Subscription serenity = subscriptionDAO.findByName(name);
                 serenity.setPrice(subscriptionDAO.sumProfileSerenity());
-                if(serenity!=null) {
-                    SubscriptionDTO serenityDTO = orikaBeanMapper.map(serenity, SubscriptionDTO.class);
-                    return serenityDTO;
-                }
+                subToReturn =   orikaBeanMapper.map(serenity, SubscriptionDTO.class);
                 break;
 
             case "confort":
                 Subscription confort = subscriptionDAO.findByName(name);
                 confort.setPrice(subscriptionDAO.sumProfileConfort());
-                if(confort!=null) {
-                    SubscriptionDTO confortDTO = orikaBeanMapper.map(confort, SubscriptionDTO.class);
-                    return confortDTO;
-                }
+                subToReturn = orikaBeanMapper.map(confort, SubscriptionDTO.class);
                 break;
 
-            case "total confort":
-                Subscription totalconfort = subscriptionDAO.findByName(name);
-                totalconfort.setPrice(subscriptionDAO.sumProfileTotalConfort());
-                if(totalconfort!=null) {
-                    SubscriptionDTO totalconfortDTO = orikaBeanMapper.map(totalconfort, SubscriptionDTO.class);
-                    return totalconfortDTO;
-                }
+            case "total-confort":
+                Subscription totalConfort = subscriptionDAO.findByName(name);
+                totalConfort.setPrice(subscriptionDAO.sumProfileTotalConfort());
+                subToReturn =  orikaBeanMapper.map(totalConfort, SubscriptionDTO.class);
                 break;
 
             default :
@@ -91,9 +87,52 @@ public class SubscriptionManagerImp implements SubscriptionManager{
        return subToReturn;
    }
 
+    //Dont work for the moment
+  /* public SubscriptionDTO sumPrice(String subscriptionName, List<String> typeList, List<String> serviceList){
+            Subscription subscription = subscriptionDAO.findByName(subscriptionName);
+            SubscriptionDTO sub = orikaBeanMapper.map(subscription, SubscriptionDTO.class);
+            double price = 0.0 ;
+
+           // List of IOT
+            String[] typeOfIOT= sub.getListofiot().split(",");
+            System.out.println(typeOfIOT);
 
 
-    }
+            for(String iot : typeOfIOT){
+                ReferencesPrices referencesPrices = referencesPricesDAO.findByType(iot);
+                price = price + referencesPrices.getPrice();
+
+            }
+
+        // List of Services
+           String[] nameOfServices= sub.getListofservice().split(",");
+           System.out.println(nameOfServices);
+
+           for(String services : nameOfServices){
+               ReferencesPrices referencesPrices = referencesPricesDAO.findByNameService(services);
+               price = price + referencesPrices.getPrice();
+
+           }
+
+        if (typeList != null){
+            for(String iot : typeList){
+                ReferencesPrices referencesPrices = referencesPricesDAO.findByType(iot);
+                price += referencesPrices.getPrice();
+
+            }
+        }
+
+            if(serviceList !=null){
+                for(String services : serviceList){
+                    ReferencesPrices referencesPrices = referencesPricesDAO.findByNameService(services);
+                    price += referencesPrices.getPrice();
+
+                }
+            }
+            sub.setPrice(price);
+        return sub;
+   }*/
+}
 
 
 
