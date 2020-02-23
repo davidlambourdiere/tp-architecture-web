e5f99ac690a0b1c36b7bf7dcba445022b1db4148
Kadia Toure <kadiditoure@gmail.com>
2020-02-23 14:15:49 +0100

    mise à jout du front pour afficher le nombre d'alerte et de resident dans la page medical.resident

--- src/main/demo-ui/src/app/components/medical.control/medical.residents/medical.residents.component.ts
+++ src/main/demo-ui/src/app/components/medical.control/medical.residents/medical.residents.component.ts
@@ -4,6 +4,8 @@ import {ActivatedRoute, Router} from "@angular/router";
 import {AuthService} from "../../../service/AuthService";
 import {PersonDTO} from "../../../dto/PersonDTO";
 import {Observable} from "rxjs";
+import {PersonStatusDTO} from "../../../dto/PersonStatusDTO";
+import {AlertHealthService} from "../../../service/AlertHealthService";
 
 @Component({
   selector: 'app-medicalresidents',
@@ -12,22 +14,48 @@ import {Observable} from "rxjs";
 })
 export class MedicalResidentsComponent implements OnInit {
   personlist: PersonDTO[];
-  constructor(private personservice: PersonService, private router: Router, private route: ActivatedRoute) {
+  residentNumber: Number;
+  newAlertsNumber: Number;
+
+  constructor(private personService: PersonService, private router: Router, private route: ActivatedRoute, private alertHealthService: AlertHealthService,) {
+    this.residentNumber =0;
+    this.newAlertsNumber=0;
   }
 
   ngOnInit() {
     this.reloadData();
+
   }
+
 
   reloadData() {
     // @ts-ignore
     this.personlist = this.findAllPeron();
+    this.findNewAlertsNumber();
+    this.findResidentNumber();
   }
 
   private findAllPeron() {
     this.route.params.subscribe(params =>{
-      this.personservice.findAllPerson().subscribe(data=>{
+      this.personService.findAllPerson().subscribe(data=>{
         this.personlist = data;
+      });
+    });
+  }
+
+  private findResidentNumber() {
+    this.route.params.subscribe(params =>{
+      this.personService.findNumberOfPersonByRole(PersonStatusDTO.RESIDENT).subscribe(data=>{
+        this.residentNumber = data;
+      });
+    });
+  }
+
+  private findNewAlertsNumber() {
+//alertNumber
+    this.route.params.subscribe(params =>{
+      this.alertHealthService.findAlertNumber().subscribe(data=>{
+        this.newAlertsNumber = data;
       });
     });
   }
