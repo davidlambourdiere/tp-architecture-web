e7092c4ede647cdb2bddc4334a46eb7c37bfedbf
Mohamed Emam <mohamed.emam93@outlook.fr>
2020-02-23 18:03:53 +0100

    Ajout du traitement d'insertion pour le profile confort

--- src/main/demo-ui/src/app/components/subscription/confort/confort.component.ts
+++ src/main/demo-ui/src/app/components/subscription/confort/confort.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {SubscriptionDTO} from "../../../dto/SubscriptionDTO";
 import {ActivatedRoute, Router} from "@angular/router";
 import {SubscriptionService} from "../../../service/SubscriptionService";
+import {SubscriptionResidentDTO} from "../../../dto/SubscriptionResidentDTO";
 
 @Component({
   selector: 'app-confort',
@@ -11,6 +12,8 @@ import {SubscriptionService} from "../../../service/SubscriptionService";
 export class ConfortComponent implements OnInit {
   subscriptions: SubscriptionDTO = new SubscriptionDTO();
   name: string;
+  login: string;
+  residentSubscriptionDTO: SubscriptionResidentDTO;
 
   constructor(private router: Router, private route: ActivatedRoute, private subservice: SubscriptionService) {
   }
@@ -21,11 +24,11 @@ export class ConfortComponent implements OnInit {
     this.showSubscriptionConfort();
   }
 
-  validate(){
+  validate() {
     this.router.navigate(["home"]);
   }
 
-  redirectToCustomPage(){
+  redirectToCustomPage() {
     this.router.navigate(["custom"]);
   }
 
@@ -34,9 +37,18 @@ export class ConfortComponent implements OnInit {
       this.subservice.findSubscriptionByProfile(this.name).subscribe(data => {
         this.subscriptions = data;
 
-      })
-    })
+      });
+    });
   }
 
+  insertSubscriptionResident() {
+    this.login = JSON.parse(localStorage.getItem('user'));
+    console.log(this.login);
+    this.route.params.subscribe(params => {
+      this.subservice.insertNewResidentInSubscription(this.subscriptions, this.login).subscribe(data => {
+        this.subscriptions = data;
+      });
+    });
+    this.validate();
+  }
 }
-
