a7731e78564213f49d63969142485c99cc176bae
Nawfal Laiouar <n.laiouar0@gmail.com>
2020-02-24 09:40:23 +0100

    Amelioration du Formulaire evolution du modéle TypeScript+Ajout de la méthode d'Inscription
    Evolution du formulaire ave la mise en place de la méthode control() à l'envoie du formulaire pour l'inscription (table= PersonProfile) des prospects + changement de la methode toggle() pour l'affichage dynamique de cahmps d'autonomie

--- src/main/demo-ui/src/app/components/survey/survey.component.ts
+++ src/main/demo-ui/src/app/components/survey/survey.component.ts
@@ -14,6 +14,10 @@ import {PersonService} from "../../service/PersonService";
 import {ActivatedRoute, Router} from "@angular/router";
 import {parseFieldArrayValue} from "@angular/compiler-cli/src/ngtsc/annotations/src/directive";
 import {importType} from "@angular/compiler/src/output/output_ast";
+import {SubscriptionService} from "../../service/SubscriptionService";
+import {PersonProfileService} from "../../service/PersonProfileService";
+import {SubscriptionDTO} from "../../dto/SubscriptionDTO";
+import {PersonProfileDTO} from "../../dto/PersonProfileDTO";
 
 function Show(addr) { document.getElementById(addr).style.visibility = "visible";
 
@@ -32,27 +36,37 @@ function Hide(addr) { document.getElementById(addr).style.visibility = "hidden";
 export class SurveyComponent implements OnInit {
 
   persons: PersonDTO[] = [];
+  personprofile: PersonProfileDTO = new PersonProfileDTO();
   loginForm: FormGroup;
   trackName: any;
   isavalable : boolean = true;
-  autonomie : boolean = true;
+
+  autonomie : number;
+
 
   //constructor(private personService: PersonService, private router: Router, private route: ActivatedRoute, private fb: FormBuilder) { }
-  constructor(private router: Router, private route: ActivatedRoute, private fb: FormBuilder){
-
+  constructor(private router: Router, private route: ActivatedRoute, private fb: FormBuilder, private personService: PersonProfileService){
   }
   ngOnInit() {
     this.loginForm = this.fb.group({
 
       Nom: ['laiouar', Validators.required],
       Prenom: ['nawfal', Validators.required],
-      Age: ['22', Validators.required],
-      Adresse: ['13 rue des Marais'],
-      Code_Postal: ['94500'],
+      Age: [22, Validators.required],
+      adress: ['13 rue des Marais', Validators.required],
+      Code_Postal: ['94500', Validators.required],
       email: ['n.laiouar0@gmail.com', Validators.required],
       Telephone: ['0781519029', Validators.required],
       autonomie: [4, Validators.required],
-      fumeur:[],
+      BAD_Autonomie: ['', Validators.required],
+      fumeur_question: [false, Validators.required],
+      fumeur: [0, Validators.required],
+      buveur_question: [false, Validators.required],
+      maladie: ['', Validators.required],
+      urgence: [false, Validators.required],
+      loisirsmodeVie: ['', Validators.required],
+      revenu: [0, Validators.required]
+
 
 
     }, {updateOn: 'blur'});
@@ -74,35 +88,46 @@ export class SurveyComponent implements OnInit {
   fumeur = new FormControl();*/
 
 
-  control(form: NgForm){
-    console.log('Données.....', form.value);
+  control(loginForm){
+    console.log('Données.....', this.loginForm.value);
+    //on transfere les valeurs des champs au DTO
+
+    this.personprofile.adress = this.loginForm.get('adress').value;
+    this.personprofile.adress_email = this.loginForm.get('email').value;
+    this.personprofile.age = this.loginForm.get('Age').value;
+    this.personprofile.chronic_disease = this.loginForm.get('maladie').value;
+    this.personprofile.drinker = this.loginForm.get('buveur_question').value;
+    this.personprofile.estimated_mobility = this.loginForm.get('autonomie').value;
+    this.personprofile.hobbies = this.loginForm.get('loisirsmodeVie').value;
+    this.personprofile.medical_emergencies = this.loginForm.get('urgence').value;
+    this.personprofile.postal_code = this.loginForm.get('Code_Postal').value;
+    this.personprofile.smoking = this.loginForm.get('fumeur_question').value;
+    this.personprofile.tel_number = this.loginForm.get('Telephone').value;
+
+
+    this.route.params.subscribe(params =>{
+    this.personService.createPersonProfile(this.personprofile).subscribe(data => {
+        this.personprofile = data;
+
+    })
+      })
   }
 
-  toggle(anId){
-    if (document.getElementById(anId).style.visibility == "hidden" ) {
-      Show(anId);
-    } else {Hide(anId);}
+
+  toggle(andId){
+
+      if(parseInt((<HTMLInputElement>document.getElementById(andId)).value) <= 5 )
+      {
+        Show("BAD_Autonomie");
+        Show("bad_otonomie");
+      }
+      else
+      {
+        Hide("BAD_Autonomie");
+        Hide("bad_otonomie");
+      }
+    }
 
   }
 
-  getCurrentModel(){
-    return JSON.stringify(this.loginForm);
-  }
-  autonomieValue(){
-    this.autonomie = false;
 
-  }
-
- //showPerson() {
-   // this.route.params.subscribe(params=> {
-     // this.personService.findAllPerson().subscribe(data=>{
-       // this.persons = data;
-     // })
-    //})
- // }
-
-  //hidePerson() {
-    //this.persons = [];
- // }
-
-}
