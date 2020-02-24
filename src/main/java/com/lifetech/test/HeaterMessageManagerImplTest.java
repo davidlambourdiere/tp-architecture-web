07bc77e862e577d98ad221d557966466fce023c9
marieXco <marie.collin98@gmail.com>
2020-02-24 09:45:50 +0100

    marie.collin - resolving conflit

--- src/main/java/com/lifetech/test/HeaterMessageManagerImplTest.java
+++ src/main/java/com/lifetech/test/HeaterMessageManagerImplTest.java
@@ -1,23 +1,23 @@
 package com.lifetech.test;
-
-import com.lifetech.application.dto.HeaterMessageDTO;
-import com.lifetech.application.manager.HeaterMessageManagerImpl;
-import com.lifetech.domain.OrikaBeanMapper;
-import com.lifetech.domain.dao.HeaterDAO;
-import com.lifetech.domain.dao.HeaterMessageDAO;
-import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-class HeaterMessageManagerImplTest {
-
-    @Test
-    public void shouldReturnLastMessageFromDataBase(){
-
-        //HeaterMessageManagerImpl heaterMessageManager = new HeaterMessageManagerImpl(heaterdao,heatermessagedao,orikaBeanMapper);
-
-        //HeaterMessageDTO output = heaterMessageManager.findFirstByOrderByInsertDateDesc();
-
-        //assertEquals(expectedOutput, output);
-    }
-}
+//
+//import com.lifetech.application.dto.HeaterMessageDTO;
+//import com.lifetech.application.manager.HeaterMessageManagerImpl;
+//import com.lifetech.domain.OrikaBeanMapper;
+//import com.lifetech.domain.dao.HeaterDAO;
+//import com.lifetech.domain.dao.HeaterMessageDAO;
+//import org.junit.jupiter.api.Test;
+//
+//import static org.junit.jupiter.api.Assertions.*;
+//
+//class HeaterMessageManagerImplTest {
+//
+//    @Test
+//    public void shouldReturnLastMessageFromDataBase(){
+//
+//        //HeaterMessageManagerImpl heaterMessageManager = new HeaterMessageManagerImpl(heaterdao,heatermessagedao,orikaBeanMapper);
+//
+//        //HeaterMessageDTO output = heaterMessageManager.findFirstByOrderByInsertDateDesc();
+//
+//        //assertEquals(expectedOutput, output);
+//    }
+//}
