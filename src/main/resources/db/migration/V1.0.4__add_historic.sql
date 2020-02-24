a3ec6646eb0e1c791b4a0011c51f5579b2796fb9
Nawfal Laiouar <n.laiouar0@gmail.com>
2020-02-24 10:06:48 +0100

    Suppression du schema de Base V1.0.5 + Mise à jour du V1.0.4
    AJout de la table lifetech.personprofile

--- src/main/resources/db/migration/V1.0.4__add_historic.sql
+++ src/main/resources/db/migration/V1.0.4__add_historic.sql
@@ -9,3 +9,18 @@ stepcounter VARCHAR(255),
 strap_id BIGINT UNSIGNED NOT NULL,
 CONSTRAINT FK_healthhistoric_strap_id FOREIGN KEY (strap_id) REFERENCES strap(id)
 );
+
+CREATE TABLE lifetech.personprofile (
+  Id SERIAL PRIMARY KEY,
+  age BIGINT NOT NULL,
+  adress varchar(300) NOT NULL,
+  postal_code int NOT NULL,
+  adress_email varchar(300) NOT NULL,
+  tel_number int NOT NULL,
+  chronic_disease varchar(300) NOT NULL,
+  estimated_mobility int NOT NULL,
+  smoking boolean NOT NULL,
+  drinker boolean NOT NULL,
+  medical_emergencies boolean NOT NULL,
+  Hobbies varchar(300) NOT NULL
+);
