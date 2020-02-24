9fabefd0c69375750c0fd777817232805e8a2336
Mohamed Emam <mohamed.emam93@outlook.fr>
2020-02-24 09:34:10 +0100

    Insertion de données pour les abonnements

--- src/main/resources/db/migration/V1.1.5__add_data.sql
+++ src/main/resources/db/migration/V1.1.5__add_data.sql
@@ -8,6 +8,10 @@ INSERT INTO lifetech.person (firstname, lastname, login, password, residence_id)
 INSERT INTO lifetech.person (firstname, lastname, login, password, residence_id) VALUES ('Mohammed', 'Henni', 'Momo', 'Momodu13', 1);
 INSERT INTO lifetech.person (firstname, lastname, login, password, residence_id) VALUES ('Henry', 'Dupont', 'Henry', 'henry', 1);
 
+INSERT INTO lifetech.person(firstname,lastname,login,password,profile_type,residence_id) VALUES ('Mohamed','Emam','mohamed','mohamed','essential',1);
+INSERT INTO lifetech.person(firstname,lastname,login,password,profile_type,residence_id) VALUES ('Lucas','Meunier','lucas','lucas','serenity',1);
+INSERT INTO lifetech.person(firstname,lastname,login,password,profile_type,residence_id) VALUES ('Tristan','Da Silva','tristan','tristan','confort',1);
+INSERT INTO lifetech.person(firstname,lastname,login,password,profile_type,residence_id) VALUES ('Alexis','Boisseau','alexis','alexis','total-confort',1);
 
 INSERT INTO lifetech.room (num) VALUES (1);
 INSERT INTO lifetech.room (num) VALUES (2);
@@ -15,24 +19,24 @@ INSERT INTO lifetech.room (num) VALUES (3);
 INSERT INTO lifetech.room (num) VALUES (4);
 INSERT INTO lifetech.room (num) VALUES (5);
 
-INSERT INTO lifetech.light (ipadress, state, breakdownstatus, percentage, color, person_id, room_id) VALUES ('172.163.92.0','ON','NOT_BREAKDOWN', '92%', 'blue', 1, 1);
+INSERT INTO lifetech.light (ipadress, state, breakdownstatus, percentage, color, person_id, room_id,price) VALUES ('172.163.92.0','ON','NOT_BREAKDOWN', '92%', 'blue', 1, 1,130);
 INSERT INTO lifetech.light (ipadress, state, breakdownstatus, percentage, color, person_id, room_id) VALUES ('172.163.92.2','ON','NOT_BREAKDOWN', '28%', 'red', 2, 2);
 
 
-INSERT INTO lifetech.heater (ipadress, state,breakdownstatus, person_id, room_id) VALUES ('172.163.92.4','ON','NOT_BREAKDOWN', 3, 3);
+INSERT INTO lifetech.heater (ipadress, state,breakdownstatus, person_id, room_id,price) VALUES ('172.163.92.4','ON','NOT_BREAKDOWN', 3, 3,150);
 INSERT INTO lifetech.heater (ipadress, state,breakdownstatus, person_id, room_id) VALUES ('172.163.92.5','OFF','NOT_BREAKDOWN', 4, 4);
 INSERT INTO lifetech.heater (ipadress, state,breakdownstatus, person_id, room_id) VALUES ('172.163.92.6','ON','NOT_BREAKDOWN', 5, 5);
 
 
-INSERT INTO lifetech.clock (ipadress, state,breakdownstatus, person_id, room_id) VALUES ('172.163.92.8','ON','NOT_BREAKDOWN', 1, 1);
+INSERT INTO lifetech.clock (ipadress, state,breakdownstatus, person_id, room_id,price) VALUES ('172.163.92.8','ON','NOT_BREAKDOWN', 1, 1,150);
 INSERT INTO lifetech.clock (ipadress, state,breakdownstatus, person_id, room_id) VALUES ('172.163.92.9','OFF','NOT_BREAKDOWN', 1, 1);
 
 
-INSERT INTO lifetech.shutter (ipadress, state,breakdownstatus, percentage, person_id, room_id) VALUES ('172.163.92.12','ON','NOT_BREAKDOWN', '50%', 3, 3);
+INSERT INTO lifetech.shutter (ipadress, state,breakdownstatus, percentage, person_id, room_id,price) VALUES ('172.163.92.12','ON','NOT_BREAKDOWN', '50%', 3, 3,70);
 INSERT INTO lifetech.shutter (ipadress, state,breakdownstatus, percentage, person_id, room_id) VALUES ('172.163.92.13','ON','NOT_BREAKDOWN', '50%', 5, 5);
 
 
-INSERT INTO lifetech.strap (ipadress, state,breakdownstatus, minsysto, maxsysto, minsteps, person_id) VALUES ('172.163.92.16','ON','NOT_BREAKDOWN', '300mg', '600mg', '300', 2);
+INSERT INTO lifetech.strap (ipadress, state,breakdownstatus, minsysto, maxsysto, minsteps, person_id,price) VALUES ('172.163.92.16','ON','NOT_BREAKDOWN', '300mg', '600mg', '300', 2,125);
 
 
 INSERT INTO lifetech.lighthistoric (startdate, endingdate,color,state,breakdownstatus,lightid) VALUES ("2020-02-24 9:00:00","2020-02-24 12:00:00","WHITE",'ON',"NOT_BREAKDOWN",1);
@@ -129,4 +133,14 @@ INSERT INTO lifetech.straphistoric (startdate, endingdate,state, breakdownstatus
 INSERT INTO lifetech.straphistoric (startdate, endingdate,state, breakdownstatus,strapid) VALUES ("2020-01-25 00:00:00","2020-02-22 00:00:00",'ON',"NOT_BREAKDOWN",1);
 INSERT INTO lifetech.straphistoric (startdate, endingdate,state, breakdownstatus,strapid) VALUES ("2020-01-24 00:00:00","2020-01-25 00:00:00",'OFF',"NOT_BREAKDOWN",1);
 
+INSERT INTO `subscription` (`id`, `optlock`, `name`,`description`, `listofiot`, `listofservice`) VALUES (NULL, '2019-12-05 00:00:00.000', 'essential','This subscription is for our residents who enjoy tranquility and security.', 'Strap,Heater', 'Sport');
+INSERT INTO `subscription` (`id`, `optlock`, `name`,`description`, `listofiot`, `listofservice`) VALUES (NULL, '2019-12-01 00:00:00.000', 'serenity', 'This subscription is intended for our resident who like pratice and love connected objects','Strap,Light,Heater', 'Bingo,Yoga');
+INSERT INTO `subscription` (`id`, `optlock`, `name`,`description`, `listofiot`, `listofservice`) VALUES (NULL, '2019-12-05 00:00:00.000', 'confort','This subscription is intended for our residents who can not stay in place and want to associate activites ans connected objects', 'Strap,Light,Heater,Shutter', 'Cultural Visite,Sport,Cinema');
+INSERT INTO `subscription` (`id`, `optlock`, `name`,`description`, `listofiot`, `listofservice`) VALUES (NULL, '2019-12-01 00:00:00.000', 'total-confort','This subscription is for our residents who need connected objects to help them in everyday life', 'Strap,Light,Shutter,Heater,Clock', 'Bingo');
 
+INSERT INTO `references_prices` (`id`, `optlock`, `description_service`, `name_service`, `num_serie`, `price`, `type`) VALUES (NULL, NULL, 'blabla', 'bingo', NULL, '25', NULL);
+INSERT INTO `references_prices` (`id`, `optlock`, `description_service`, `name_service`, `num_serie`, `price`, `type`) VALUES (NULL, NULL, 'blabla', 'cinéma', NULL, '100', NULL);
+INSERT INTO `references_prices` (`id`, `optlock`, `description_service`, `name_service`, `num_serie`, `price`, `type`) VALUES (NULL, NULL, 'blabla', 'sport', NULL, '45', NULL);
+INSERT INTO `references_prices` (`id`, `optlock`, `description_service`, `name_service`, `num_serie`, `price`, `type`) VALUES (NULL, NULL, 'blabla', 'yoga', NULL, '30', NULL);
+INSERT INTO `references_prices` (`id`, `optlock`, `description_service`, `name_service`, `num_serie`, `price`, `type`) VALUES (NULL, NULL, 'blabla', 'viste culturel', NULL, '50', NULL);
+UPDATE `references_prices` SET `num_serie` = 'S01' WHERE `references_prices`.`id` = 1; UPDATE `references_prices` SET `num_serie` = 'A01' WHERE `references_prices`.`id` = 2; UPDATE `references_prices` SET `num_serie` = 'A01' WHERE `references_prices`.`id` = 3; UPDATE `references_prices` SET `num_serie` = 'S01' WHERE `references_prices`.`id` = 4; UPDATE `references_prices` SET `num_serie` = 'A01' WHERE `references_prices`.`id` = 5;
