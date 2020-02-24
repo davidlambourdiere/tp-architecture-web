87d2e7c64fca462c5e9a7e5b44447f05b7b3cf1f
marieXco <marie.collin98@gmail.com>
2020-02-24 15:07:33 +0100

    marie.collin - Deleting useless classes

--- src/main/java/com/lifetech/api/rest/HeaterBreakdownDetectionController.java
+++
@@ -1,7 +0,0 @@
-package com.lifetech.api.rest;
-
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class HeaterBreakdownDetectionController extends RestBaseController {
-}
