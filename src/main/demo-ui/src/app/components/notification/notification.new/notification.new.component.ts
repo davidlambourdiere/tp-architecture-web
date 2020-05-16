455102fbe25d992f405bc9bf516c371d3890d69a
LouisBeaudin <louisbeaudin@outlook.fr>
2020-05-16 12:17:17 +0200

    ajout de l'envoi de la notification

--- src/main/demo-ui/src/app/components/notification/notification.new/notification.new.component.ts
+++ src/main/demo-ui/src/app/components/notification/notification.new/notification.new.component.ts
@@ -4,6 +4,17 @@ import {ActivatedRoute, Router} from "@angular/router";
 import {AuthService} from "../../../service/AuthService";
 import {NotificationService} from "../../../service/NotificationService";
 import {PersonService} from "../../../service/PersonService";
+import {FormControl} from "@angular/forms";
+import {ShutterService} from "../../../service/ShutterService";
+import {LightService} from "../../../service/LightService";
+import {ClockService} from "../../../service/ClockService";
+import {StrapService} from "../../../service/StrapService";
+import {HeaterService} from "../../../service/HeaterService";
+import {HeaterDTO} from "../../../dto/HeaterDTO";
+import {ShutterDTO} from "../../../dto/ShutterDTO";
+import {LightDTO} from "../../../dto/LightDTO";
+import {ClockDTO} from "../../../dto/ClockDTO";
+import {StrapDTO} from "../../../dto/StrapDTO";
 
 @Component({
   selector: 'notification/new',
@@ -12,8 +23,25 @@ import {PersonService} from "../../../service/PersonService";
 })
 export class NotificationNewComponent implements OnInit {
 
-  userConnected: PersonDTO;
-  allusers:PersonDTO[];
+  userConnected: PersonDTO = null;
+  allusers:PersonDTO[] =null;
+  myControl = new FormControl();
+  personneconcerning: string;
+  destinataire:string = '';
+  formtype: string = '';
+  concerningobject: string;
+  objecttype: string;
+  heater: HeaterDTO;
+  shutter: ShutterDTO;
+  light: LightDTO;
+  clock: ClockDTO;
+  strap: StrapDTO;
+  id: string;
+  allHeater: HeaterDTO[];
+  allShutter: ShutterDTO[];
+  allClock: ClockDTO[];
+  allLight: LightDTO[];
+  allStrap: StrapDTO[];
 
   ngOnInit(): void {
     this.getUserConnected();
@@ -21,7 +49,8 @@ export class NotificationNewComponent implements OnInit {
   }
 
   constructor(private router: Router, private route: ActivatedRoute, private authService: AuthService,
-              private notificationService: NotificationService, private personService: PersonService) {
+              private notificationService: NotificationService, private personService: PersonService, private shutterService: ShutterService,
+              private lightService: LightService, private clockService: ClockService, private strapService:StrapService, private heaterService: HeaterService) {
   }
 
   getLogin() {
@@ -40,6 +69,46 @@ export class NotificationNewComponent implements OnInit {
     this.route.params.subscribe(params => {
       this.personService.findAllPerson().subscribe(data => {
         this.allusers = data;
+      })
+    })
+  }
+
+  findLight(): void{
+    this.route.params.subscribe(params => {
+      this.lightService.findLight(this.id).subscribe(data => {
+        this.light = data;
+      })
+    })
+  }
+
+  findHeater(): void{
+    this.route.params.subscribe(params => {
+      this.heaterService.findHeater(this.id).subscribe(data => {
+        this.heater = data;
+      })
+    })
+  }
+
+  findClock(): void{
+    this.route.params.subscribe(params => {
+      this.clockService.findClock(this.id).subscribe(data => {
+        this.clock = data;
+      })
+    })
+  }
+
+  findShutter(): void{
+    this.route.params.subscribe(params => {
+      this.shutterService.findShutter(this.id).subscribe(data => {
+        this.shutter = data;
+      })
+    })
+  }
+
+  findStrap(): void{
+    this.route.params.subscribe(params => {
+      this.strapService.findStrap(this.id).subscribe(data => {
+        this.strap = data;
       })
     })
   }
--- src/main/demo-ui/src/app/components/notification/notification.shutter/notification.shutter.component.ts
+++ src/main/demo-ui/src/app/components/notification/notification.shutter/notification.shutter.component.ts
@@ -5,6 +5,10 @@ import {LightService} from "../../../service/LightService";
 import {ShutterDetailDTO} from "../../../dto/ShutterDetailDTO";
 import {ShutterDTO} from "../../../dto/ShutterDTO";
 import {ShutterService} from "../../../service/ShutterService";
+import {NotificationService} from "../../../service/NotificationService";
+import {AuthService} from "../../../service/AuthService";
+import {NotificationDTO} from "../../../dto/NotificationDTO";
+import {PersonDTO} from "../../../dto/PersonDTO";
 
 @Component({
   selector: 'notification/shutter',
@@ -14,20 +18,55 @@ import {ShutterService} from "../../../service/ShutterService";
 export class NotificationShutterComponent implements OnInit{
 
   id: string;
+  formtype: string = '';
+  concerningobject: string;
+  personneconcerning:string;
   shutter: ShutterDTO;
+  message: NotificationDTO;
+  comment: string ='';
+  userConnected: PersonDTO = null;
 
-  constructor(private router: Router, private route: ActivatedRoute, private shutterService: ShutterService) {
+  constructor(private router: Router, private route: ActivatedRoute, private shutterService: ShutterService,
+              private notificationService: NotificationService, private authService: AuthService) {
   }
 
   ngOnInit(): void {
     this.id = this.route.snapshot.paramMap.get('id');
     this.findShutter();
+    this.getUserConnected();
   }
 
   findShutter(): void{
     this.route.params.subscribe(params => {
       this.shutterService.findShutter(this.id).subscribe(data => {
         this.shutter = data;
+      })
+    })
+  }
+
+  getLogin() {
+    return this.authService.getUser().login;
+  }
+  getUserConnected(): void{
+    this.route.params.subscribe(params => {
+      this.notificationService.findUser(this.getLogin()).subscribe(data => {
+        this.userConnected = data;
+      })
+    })
+  }
+
+  constructMessageToSend(): void{
+    this.message = new NotificationDTO();
+    this.message.destinataire = this.shutter.person.login;
+    this.message.emetteur = this.userConnected.login;
+    this.message.message = this.comment;
+  }
+
+  sendMessage(): void{
+    this.constructMessageToSend();
+    this.route.params.subscribe(params =>{
+      this.notificationService.sendMessage(this.message).subscribe(data => {
+        this.message = data;
       })
     })
   }
