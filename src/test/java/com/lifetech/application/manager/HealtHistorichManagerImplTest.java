5faff9dee88c823f08ca49ab2b415497052f801e
Kadia Toure <kadiditoure@gmail.com>
2020-05-17 11:48:31 +0200

    back: suppression d'un test

--- src/test/java/com/lifetech/application/manager/HealtHistorichManagerImplTest.java
+++ src/test/java/com/lifetech/application/manager/HealtHistorichManagerImplTest.java
@@ -57,59 +57,5 @@ public class HealtHistorichManagerImplTest {
         assertNotNull("method must be successfully invoked", h);
     }
 
-    /***
-     * Init: Create 1 insatnce of healthHistoricDAO 1 instance of cache Map<Long, Map<String, AlertCache>> and 4 instance of HealthHistoric with alert
-     * invoke alertDetection 4 times for each healthHistoric after saved them
-     * verification : Must return isAlert at false 3 times before isAlert at True
-     ****/
-   /* @Test
-    public void testAlertDetection_TheMethodMustToBeSuccessfullyInvokedAndReturnTrue() {
-        // init
-        Map<Long, Map<String, AlertCache>> cache=new HashMap<>();
-        ArrayList<HealthHistoric> list = new ArrayList<>();
 
-        HealthHistoric histSaved1 = new HealthHistoric();
-        histSaved1.setHearthrate("197");
-        histSaved1.setStrap((long) 8);
-        list.add(histSaved1);
-
-        HealthHistoric histSaved2 = new HealthHistoric();
-        histSaved2.setHearthrate("198");
-        histSaved2.setStrap((long) 8);
-        list.add(histSaved2);
-
-        HealthHistoric histSaved3 = new HealthHistoric();
-        histSaved3.setHearthrate("199");
-        histSaved3.setStrap((long) 8);
-        list.add(histSaved3);
-
-        HealthHistoric histSaved4 = new HealthHistoric();
-        histSaved4.setHearthrate("200");
-        histSaved4.setStrap((long) 8);
-        list.add(histSaved4);
-
-
-        Mockito.when(healthHistoricDAO.save(histSaved1)).thenReturn(histSaved1);
-        Mockito.when(healthHistoricDAO.save(histSaved2)).thenReturn(histSaved2);
-        Mockito.when(healthHistoricDAO.save(histSaved3)).thenReturn(histSaved3);
-        Mockito.when(healthHistoricDAO.save(histSaved4)).thenReturn(histSaved4);
-        //Mockito.when( healthHistoricDAO.findByStrap((long) 8)).thenReturn(list);
-
-        // transition
-        healthHistoricDAO.save(histSaved1);
-        boolean isAlert1 = healthHistoricManagerImpl.alertDetection(histSaved1, cache);
-        healthHistoricDAO.save(histSaved2);
-        boolean isAlert2 =healthHistoricManagerImpl.alertDetection(histSaved2, cache);
-        healthHistoricDAO.save(histSaved3);
-        boolean isAlert3 = healthHistoricManagerImpl.alertDetection(histSaved3, cache);
-        healthHistoricDAO.save(histSaved4);
-        boolean isAlert4 = healthHistoricManagerImpl.alertDetection(histSaved4, cache);
-
-        // verification
-        assertFalse("alert not detected", isAlert1);
-        assertFalse("alert not detected", isAlert2);
-        assertFalse("alert not detected", isAlert3);
-       // assertTrue("alert detected", isAlert4);
-    }
-*/
 }
