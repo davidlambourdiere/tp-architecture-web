030ac2e71f901ef9f9101f3373d7e3f79d9b7cc8
David LAMBOURDIERE <davidlambourdiere@gmail.com>
2020-05-18 12:48:29 +0200

    update IOT DAO so they can count iots by residence id or personid

--- src/main/java/com/lifetech/domain/dao/LightDAO.java
+++ src/main/java/com/lifetech/domain/dao/LightDAO.java
@@ -5,6 +5,7 @@ import com.lifetech.domain.model.Person;
 import com.lifetech.domain.model.Residence;
 import com.lifetech.domain.model.Room;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -17,4 +18,10 @@ public interface LightDAO  extends JpaRepository<Light, Long> {
     Long countByPerson(Person person);
 
     List<Light> findByRoom(Room room);
+
+    @Query(value="select count(*) from light where light.person_id = ?1 ;", nativeQuery=true)
+    String countByPersonId(String personid);
+
+    @Query(value="select count(*) from light join person on person.id = light.person_id where person.residence_id = ?1 ;", nativeQuery=true)
+    String countByResidenceId(String residenceid);
 }
