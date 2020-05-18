3a3f3a9709af231299f1e7509879937cb4f4c712
Kadia Toure <kadiditoure@gmail.com>
2020-05-18 10:46:19 +0200

    resolution conflit dependances angular

--- src/main/demo-ui/src/app/service/LightService.ts
+++ src/main/demo-ui/src/app/service/LightService.ts
@@ -1,6 +1,7 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
+import {PersonDTO} from '../dto/PersonDTO';
 import {LightDTO} from "../dto/LightDTO";
 
 @Injectable({
@@ -10,28 +11,27 @@ export class LightService {
 
   constructor(private http: HttpClient) {
   }
-  findIOTByRoom(idroom: string): Observable<any> {
-    return this.http.get(`api/light/findByRoom/${idroom}`);
+
+
+  findLight(id: string) : Observable<any> {
+    return this.http.get(`api/light/find/${id}`);
   }
-
-
 
   findbyId(id: bigint) :Observable<LightDTO>{
     return this.http.get<LightDTO>(`api/light/${id}`);
   }
 
-<<<<<<< HEAD
   updateLight(id: bigint , light: LightDTO): Observable<LightDTO> {
     return this.http.put<LightDTO>(`api/light/updateLight/${id}`, light);
   }
-=======
-  // updateLight(id: bigint , light: LightDTO): Observable<LightDTO> {
-   // return this.http.put<LightDTO>(`api/light/updateLight/${id}`, light);
-  // }
->>>>>>> parent of 33782fd... changements mineurs
 
 
-  updateLight(id: number , light: LightDTO): Observable<LightDTO> {
+  updateLightNotification(id: number , light: LightDTO): Observable<LightDTO> {
     return this.http.put<LightDTO>(`api/light/updateLight/${id}`, light);
+  }
+
+
+  findIOTByRoom(idroom: string): Observable<any> {
+    return this.http.get(`api/light/findByRoom/${idroom}`);
   }
 }
